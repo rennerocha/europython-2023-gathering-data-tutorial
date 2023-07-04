1f6ef3b2d8698ee24151c5d4146720671b6466f5
Renne Rocha <renne@rennerocha.com>
2023-07-03 22:44:55 -0300

    Remove solutions

--- code/exercise-2.py
+++ code/exercise-2.py
@@ -16,27 +16,11 @@ import scrapy
 class QuotesScrollSpider(scrapy.Spider):
     name = "quotes_scroll"
     allowed_domains = ["quotes.toscrape.com"]
-    api_url = "https://quotes.toscrape.com/api/quotes?page={page}"
 
     def start_requests(self):
-        yield scrapy.Request(self.api_url.format(page=1))
+        # TODO
+        ...
 
     def parse(self, response):
-        data = response.json()
-        actual_page = data.get("page")
-
-        for quote in data.get("quotes"):
-            yield {
-                "quote": quote.get("text"),
-                "author": quote.get("author").get("name"),
-                "author_url": response.urljoin(
-                    quote.get("author").get("goodreads_link")
-                ),
-                "tags": quote.get("tags"),
-            }
-
-        if data.get("has_next"):
-            next_page = actual_page + 1
-            yield scrapy.Request(
-                self.api_url.format(page=next_page),
-            )
+        # TODO
+        ...
--- code/exercise-4.py
+++ code/exercise-4.py
@@ -8,39 +8,18 @@ class QuotesViewStateSpider(scrapy.Spider):
 
     def parse(self, response):
         authors = response.css("#author option::attr(value)").getall()
-        view_state = response.css("#__VIEWSTATE::attr(value)").get()
 
+        form_data = {
+            # TODO
+        }
         for author in authors:
             yield scrapy.FormRequest(
                 response.urljoin(response.css("form::attr(action)").get()),
                 callback=self.parse_author_tags,
-                formdata={
-                    "__VIEWSTATE": view_state,
-                    "author": author,
-                },
+                formdata=form_data,
                 cb_kwargs={"author": author}
             )
 
     def parse_author_tags(self, response, author):
-        tags = response.css("#tag option::attr(value)").getall()
-        view_state = response.css("#__VIEWSTATE::attr(value)").get()
-
-        for tag in tags:
-            yield scrapy.FormRequest(
-                response.urljoin(response.css("form::attr(action)").get()),
-                callback=self.parse_tag_results,
-                formdata={
-                    "__VIEWSTATE": view_state,
-                    "author": author,
-                    "tag": tag,
-                },
-            )
-
-    def parse_tag_results(self, response):
-        quotes = response.css(".quote")
-        for quote in quotes:
-            yield {
-                "quote": quote.css(".content::text").get(),
-                "author": quote.css(".author::text").get(),
-                "tag": quote.css(".tag::text").get(),
-            }
+        # TODO
+        ...
