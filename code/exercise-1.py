4df146c52a1d947842149282fb9671293a232a10
Renne Rocha <renne@rennerocha.com>
2023-07-16 06:24:19 -0300

    Add solutions

--- code/exercise-1.py
+++ code/exercise-1.py
@@ -18,5 +18,17 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ["https://quotes.toscrape.com"]
 
     def parse(self, response):
-        # TODO
-        ...
+        quotes = response.css(".quote")
+        for quote in quotes:
+            yield {
+                "quote": quote.css(".text::text").get(),
+                "author": quote.css(".author::text").get(),
+                "author_url": response.urljoin(
+                    quote.css("span a::attr(href)").get()
+                ),
+                "tags": quote.css(".tag *::text").getall(),
+            }
+
+        yield scrapy.Request(
+            response.urljoin(response.css(".next a::attr(href)").get())
+        )
