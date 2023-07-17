e1f921b52f95388bfb6bb11a1e2841266e434f6e
Renne Rocha <renne@rennerocha.com>
2023-07-17 18:48:28 -0300

    Sample project with monitoring

---
+++ code/monitoring/monitoring/monitors.py
@@ -0,0 +1,24 @@
+from spidermon import Monitor, MonitorSuite, monitors
+from spidermon.contrib.actions.reports.files import CreateFileReport
+
+
+@monitors.name("Item count")
+class ItemCountMonitor(Monitor):
+    @monitors.name("Minimum number of items")
+    def test_minimum_number_of_items(self):
+        item_extracted = getattr(self.data.stats, "item_scraped_count", 0)
+        minimum_threshold = 10
+
+        msg = "Extracted less than {} items".format(minimum_threshold)
+        self.assertTrue(item_extracted >= minimum_threshold, msg=msg)
+
+
+class SpiderCloseMonitorSuite(MonitorSuite):
+
+    monitors = [
+        ItemCountMonitor,
+    ]
+
+    monitors_finished_actions = [
+        CreateFileReport,
+    ]
